a177d0ac78727ff40b09a4d900be6a5781f13375
unknown <johanacent@gmail.com>
2023-06-09 16:56:27 -0300

    Agregando la tabla de usuario

--- SEMANA7/desafio_07.sql
+++ SEMANA7/desafio_07.sql
@@ -13,3 +13,25 @@ CREATE TABLE IF NOT EXISTS comentario (
     Foreign Key (id_articulo) REFERENCES (articulo),
 
 ) 
+
+CREATE TABLE IF NOT EXISTS usuario(
+    id_usuario INT (8) NOT NULL,
+    nombre VARCHAR (15),
+    apellido VARCHAR (15),
+    telefono INT (10),
+    username VARCHAR (10),
+    email VARCHAR (25)
+    contraseña CHAR(25)
+    estado BOOLEAN,
+    fecha_creacion DATE,
+    avatar CHAR (25)
+    es_publico BOOLEAN,
+    es_colaborador BOOLEAN,
+    es_admin BOOLEAN,
+    PRIMARY KEY (id_usuario)
+)
+
+--Agregar el comando necesario que introduzca en la tabla usuario, 1 usuario con rol de admin, 4 con rol de colaborador y 5 con rol de público.
+-- Donde los campos: es_publico, es_colaborador y es_admin son booleanos.--
+
+INSERT INTO usuario (es_admin)
