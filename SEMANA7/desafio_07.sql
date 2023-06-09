caac67a9b765b0da0221178222a03f128232e836
Johana <131535999+JbhC@users.noreply.github.com>
2023-06-09 16:28:03 -0300

    Update desafio_07.sql

--- SEMANA7/desafio_07.sql
+++ SEMANA7/desafio_07.sql
@@ -4,11 +4,12 @@ USE desafio_07;
 
 CREATE TABLE IF NOT EXISTS comentario (
 
-    id INT (25) PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+    id INT (25) NOT NULL AUTO_INCREMENT, 
     id_articulo INT (6),
     id_usuario INT (4),
     contenido VARCHAR(250),
     fecha_hora TIMESTAMP,
+    PRIMARY KEY (id),
     Foreign Key (id_usuario) REFERENCES (usuario),
     Foreign Key (id_articulo) REFERENCES (articulo),
 
