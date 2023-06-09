da5af1e06576ce957c15491b52ac659982f3fe36
unknown <johanacent@gmail.com>
2023-06-09 05:59:58 -0300

    Creación de la tabla comentario con sus campos

--- SEMANA7/desafio_07.sql
+++ SEMANA7/desafio_07.sql
@@ -2,12 +2,14 @@ CREATE DATABASE desafio_07;
 
 USE desafio_07;
 
-CREATE TABLE comentario (
+CREATE TABLE IF NOT EXISTS comentario (
 
-    id INT PRIMARY KEY, 
-    id_articulo int(6),
+    id INT (25) PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+    id_articulo INT (6),
     id_usuario INT (4),
-    contenido VARCHAR(255),
-    fecha_hora DATETIME,
+    contenido VARCHAR(250),
+    fecha_hora TIMESTAMP,
+    Foreign Key (id_usuario) REFERENCES (usuario),
+    Foreign Key (id_articulo) REFERENCES (articulo),
 
-) ,
+) 
