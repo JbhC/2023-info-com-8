4fb76edfca6050652020575cd4befa8a0d7b3209
eugenyan <131491291+eugenyan@users.noreply.github.com>
2023-06-12 12:26:59 -0300

    Desafio_08.py

---
+++ Desafio_08.py
@@ -0,0 +1,54 @@
+# Crear las siguientes clases con sus atributos y métodos.
+
+# Clase Usuario
+## atributos: id, nombre, apellido, teléfono, username, email, contraseña, fecha de registro, avatar, estado, online
+class Usuario:
+    def _init_ (self, id, nombre, apellido, telefono, username, email, contraseña, fecha_registro, avatar, estado, online):
+        self.id = id
+        self.nombre = nombre
+        self.apellido = apellido
+        self.telefono = telefono
+        self.username = username
+        self.email = email
+        self.contraseña = contraseña
+        self.fecha_registro = datetime.now()
+        self.avatar = None
+        self.estado = "Activo"
+        self.online = False #Clau
+        
+### Metodos login(), registrar()
+    def login(self, username, password): 
+        self.username = input("Ingresar Username: ")
+        self.contraseña = input("Ingresar Contraseña: ")
+        if username == username and password == password:
+            self.online = True
+        return f"Bienvenido/a! {self.nombre} has iniciado sesion correctamente" #Euge
+    
+    def registrar(self):
+      ## definir
+        return f"Hola! {self.nombre} te has registrado correctamente!" #Clau
+
+# Clase Publico(Usuario)
+## atributo: es_publico
+class Publico(Usuario):
+    def __init__(self, id, nombre, apellido, telefono, username, email, contraseña, fecha_registro, avatar, estado, es_publico):
+        super().__init__(id, nombre, apellido, telefono, username, email, contraseña, fecha_registro, avatar, estado)
+        self.es_publico = es_publico
+        
+    ### métodos: comentar(), registrar() -> se define en Usuario por lo que entendi(?
+    def comentar(self, articulo, comentario):
+        # definir
+        pass
+
+# clase Colaborador(Usuario)
+## atributos: es_colaborador
+
+### métodos: registrar(), comentar(), publicar()
+
+
+# clase Articulo
+## id, id_usuario, titulo, resumen, contenido, fecha_publicacion, imagen, estado
+
+
+# clase Comentario
+## id, id_articulo, id_usuario, contenido, fecha_hora, estado
